885fb74a217985e23c4aa494f688e7728a0ed424
Prashant Dawkhar <dawkharp79@gmail.com>
2024-04-24 08:22:46 +0530

    Code changes

--- currying.js
+++ currying.js
@@ -1,24 +1,43 @@
-// fuction currying using bind method
-// multiply
-
-function multiply(a, b) {
-    return a*b;
-}
-
-let multiplyByTwo = multiply.bind(this, 2);
-console.log(multiplyByTwo(4));
-
-let multiplyByThree = multiply.bind(this, 3);
-console.log(multiplyByThree(6));
-
-
-// fuction currying using closures
-// multiply
-function multiplication(a) {
-    return function (b) {
-        return a * b
+let limit = 5;
+function add(...args) {
+    if(args.length === limit) {
+        let result = args.reduce((acc, curr) => acc + curr, 0);
+        return result
+    } else {
+        return function (...cargs) {
+            args = args.concat(cargs);
+            return add(...args)
+        }
     }
-
 }
 
-console.log(multiplication(5)(10))
+console.log(add(1, 2, 3, 4, 5));
+console.log(add(1, 2, 3, 4)(5));
+console.log(add(1, 2, 3)(4, 5));
+console.log(add(1, 2, 3)(4)(5));
+console.log(add(1)(2)(3)(4)(5));
+
+// // fuction currying using bind method
+// // multiply
+
+// function multiply(a, b) {
+//     return a*b;
+// }
+
+// let multiplyByTwo = multiply.bind(this, 2);
+// console.log(multiplyByTwo(4));
+
+// let multiplyByThree = multiply.bind(this, 3);
+// console.log(multiplyByThree(6));
+
+
+// // fuction currying using closures
+// // multiply
+// function multiplication(a) {
+//     return function (b) {
+//         return a * b
+//     }
+
+// }
+
+// console.log(multiplication(5)(10))
