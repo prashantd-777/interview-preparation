2a840bb69de68d26529c3e06b38782bd9cb7f057
Prashant love coding <dawkharp79@gmail.com>
2025-08-05 22:04:55 +0530

    Added changes to the closure functions

--- closure.js
+++ closure.js
@@ -1,3 +1,33 @@
+// Function are bundled together with references to it's surrounding environment (lexical scope), that nothing but the closures.
+
+// Basic Example
+// function x() {
+//     var a = 7;
+//     function y() {
+//         console.log(a);
+//     }
+//     y();
+// }
+// x();
+
+// Another example => When we return function from another function, so that time we can call this function any time below in our code and also it remembers the reference and values into that scope.
+function x() {
+    var a = 7;
+    function y() {
+        console.log(a);
+    }
+    return y;
+}
+var z = x();
+console.log(z);
+// .........
+// 100 lines of code
+// .....
+// ...
+z();
+
+
+// Another example ==> Increment the value of {x} using closure reference
 function outerFunction() {
     let x = 10;
     return function innerFunction() {
