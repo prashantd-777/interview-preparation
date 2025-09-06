768de706b90c1b8397c046c88a04930234de120c
Prashant love coding <dawkharp79@gmail.com>
2025-09-06 11:36:59 +0530

    Code changs

--- pollyfills/index.js
+++ pollyfills/index.js
@@ -19,11 +19,10 @@ const Users = {
     lastName: "Wick",
 }
 
-console.log(getMyDetails.call(Users, "30", "Mumbai", "engineer"));
-console.log(getMyDetails.apply(Users, ["30", "Mumbai", "engineer"]));
-let b1 = getMyDetails.bind(Users, "30", "Mumbai");
-console.log(b1("engineer"));
-
+// console.log(getMyDetails.call(Users, "30", "Mumbai", "engineer"));
+// console.log(getMyDetails.apply(Users, ["30", "Mumbai", "engineer"]));
+// let b1 = getMyDetails.bind(Users, "30", "Mumbai");
+// console.log(b1("engineer"));
 
 Function.prototype.myCall = function (scope, ...args) {
     scope._this = this;
@@ -42,13 +41,85 @@ Function.prototype.myBind = function(scope, ...args) {
     }
 }
 
-console.log(getMyDetails.myCall(Users, "30", "Mumbai", "engineer"));
-console.log(getMyDetails.myApply(Users, ["30", "Mumbai", "engineer"]));
-let b2= getMyDetails.myBind(Users, "30", "Mumbai");
-console.log("b2 is:", b2("engineer"));
+// console.log(getMyDetails.myCall(Users, "30", "Mumbai", "engineer"));
+// console.log(getMyDetails.myApply(Users, ["30", "Mumbai", "engineer"]));
+// let b2= getMyDetails.myBind(Users, "30", "Mumbai");
+// console.log("b2 is:", b2("engineer"));
 
+function dummyAPI(time) {
+    return new Promise((res, rej) => {
+        setTimeout(() => res(time), time);
+    })
+}
 
+const promiseArray = [dummyAPI(1000), dummyAPI(2000), dummyAPI(3000)];
 
+// Promise.all(promiseArray).then((res, rej) => {
+//     console.log('res', res);
+// })
 
+// Promise.race(promiseArray).then((res, rej) => {
+//     console.log('race promise res', res);
+// })
 
+Promise.allSettled(promiseArray).then((res, rej) => {
+    console.log("settled promise res", res);
+})
 
+Promise.myAllSettled = function (pArray) {
+    let mPromise = pArray.map(item => {
+        return Promise.resolve(item).then(val => {
+            return {
+                status: 'fulfilled',
+                value: val
+            }
+        }).catch(err => {
+            return {
+                status: "error",
+                reason: err
+            }
+        })
+    });
+
+    return Promise.all(mPromise);
+}
+
+Promise.myAllSettled(promiseArray).then(val => {
+    console.log("val is::", val)
+})
+
+Promise.myRace = function (pArray) {
+    const promise = new Promise((res, rej) => {
+        pArray.forEach(item => {
+            Promise.resolve(item).then(res).catch(rej);
+        })
+    })
+    return promise;
+}
+
+// Promise.myRace(promiseArray).then((res, rej) => {
+//     console.log('race promise res', res);
+// })
+
+Promise.myAll = function (pArray) {
+    const output = [];
+    let promise = new Promise((res, rej) => {
+        pArray.forEach(async(item,index) => {
+            try {
+                const result = await item;
+                output.push(result);
+                if(pArray.length - 1 === index) {
+                    res(output);
+                }
+            } catch (err) {
+                rej(err);
+            }
+        })
+    })
+
+    return promise;
+}
+
+// Promise.myAll(promiseArray).then((res, rej) => {
+//     console.log('res', res);
+// })
