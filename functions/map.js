10d2a504afda5695c5d039dd4ff8e6b2dfce84f4
Prashant love coding <dawkharp79@gmail.com>
2025-09-06 09:52:12 +0530

    Code changes

--- functions/map.js
+++ functions/map.js
@@ -19,3 +19,31 @@ const result1 = inputArray.myMap(double);
 
 console.log('result', result);
 console.log('result1', result1);
+
+
+
+const result2 = [26, 75, 50, 26].reduce((acc, current) => {
+    acc[current] = acc[current] ? acc[current] + 1 : 1;
+    return acc;
+}, {});
+
+
+Array.prototype.myReduce = function (cb, current) {
+    let acc = current;
+    for(let i = 0; i< this.length; i++) {
+        if(acc) {
+            acc = cb(acc, this[i]);
+        } else {
+            acc = this[i]
+        }
+    }
+
+    return acc;
+}
+
+const result3 = [26, 75, 50, 26].myReduce((acc, current) => {
+    acc[current] = acc[current] ? acc[current] + 1 : 1;
+    return acc;
+}, {});
+
+console.log('result3', result3);
